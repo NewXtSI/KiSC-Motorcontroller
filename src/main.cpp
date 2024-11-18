18cd940b4dc0aea6a1768df6bd4c750f87c4cf2a
Henning Schreiber <henning.schreiber@wago.com>
2024-11-18 13:23:08 +0100

    First source

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "../KiSC-ESP-Now-Protocol/include/kisc-espnow.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+
+}
