9636ae72a647be4b8ab17badd16450f48874414f
Henning Schreiber <henning.schreiber@wago.com>
2024-11-18 14:06:02 +0100

    add_first

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
 #include "../KiSC-ESP-Now-Protocol/include/kisc-espnow.h"
+#include "../KiSC-hoverboard-protocol/include/kisc-hoverboard-protocol.h"
 
 void setup() {
   // put your setup code here, to run once:
